d53dea6a9c60aeeaffcd56fbc173052d160c1d5b
Esteban Quintana <a01184855@itesm.mx>
2017-07-20 14:34:18 -0500

    Adding points to array

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,15 @@
 var app = angular.module("app", []);
 app.controller("appctrl", function($scope){
     $scope.appTitle = "Linear interpolation";
+    $scope.point = {x: null, y: null};
+    $scope.points = [];
+
+    $scope.submitPoint = function () {
+      point = {
+        x: $scope.point.x,
+        y: $scope.point.y
+      }
+      $scope.points.push(point);
+    };
+
 });
